ed81dc48b9f0a319ca5a382376a1325c89753ade
Ирина <mi.irene97@gmail.com>
2023-02-20 19:30:44 +0300

    Initial commit

---
+++ src/main/java/com/example/interviewtasks/InterviewTasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/interviewtasks/InterviewTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
